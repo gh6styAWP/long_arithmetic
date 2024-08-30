60c11867f813431cf7c7e8f5934c1fceddfc7fc7
gh6sty6@mail.ru <gh6sty6@mail.ru>
2024-08-30 15:26:32 +0300

    Добавьте файлы проекта.

---
+++ long_arithmetic/long_arithmetic.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    vector<int> a;
+    vector<int> b;
+    string input;
+
+    cout << "Введите числа для числа a, разделенные пробелами, и нажмите enter: ";
+    getline(cin, input);
+    istringstream stream(input);
+    int num;
+    while (stream >> num) {
+        a.push_back(num);
+    }
+
+    cout << "\nВектор 1: ";
+    for (auto x : a)
+        cout << x << " ";
+
+    cout << "\nВведите числа для числа b, разделенные пробелами, и нажмите enter: ";
+    getline(cin, input);
+    istringstream stream2(input);
+    while (stream2 >> num) {
+        b.push_back(num);
+    }
+
+    cout << "\nВектор 2: ";
+    for (auto x : b)
+        cout << x << " ";
+
+}
+
