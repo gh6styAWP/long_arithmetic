e66ab5a523b89cf729bdd27ec5c7fedb7ef93089
gh6sty6@mail.ru <gh6sty6@mail.ru>
2024-09-01 23:09:45 +0300

    Реализована функция сложения длинных чисел

--- long_arithmetic/long_arithmetic.cpp
+++ long_arithmetic/long_arithmetic.cpp
@@ -1,6 +1,5 @@
 ﻿#include <iostream>
 #include <vector>
-#include <sstream>
 #include <string>
 using namespace std;
 
@@ -30,21 +29,44 @@ void print_vector(const vector<int>& num_vector) {
     cout << endl;
 }
 
+//функция сложения
+vector<int> sum_long_numbers(const vector<int>& a, const vector<int>& b) {
+    vector<int> result;
+    int carry = 0;
+    int a_size = a.size();
+    int b_size = b.size();
+    int max_size = max(a_size, b_size);
+
+    for (int i = 0; i < max_size; i++) {
+        int digit_a = (i < a_size) ? a[a_size - 1 - i] : 0;
+        int digit_b = (i < b_size) ? b[b_size - 1 - i] : 0;
+
+        int sum = digit_a + digit_b + carry;
+        carry = sum / 10;
+        result.push_back(sum % 10);
+    }
+
+    if (carry > 0) {
+        result.push_back(carry);
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+
 int main()
 {
     setlocale(LC_ALL, "Ru");
     vector<int> a = read_long_number();
     vector<int> b = read_long_number();
 
-    cout << "\nЧисло 1: ";
-    print_vector(a);
+    
 
-    cout << "\nЧисло 2: ";
-    print_vector(b);
+    vector<int> sum = sum_long_numbers(a, b);
+    cout << "\nСумма чисел: ";
+    print_vector(sum);
 
-
-
-
-
+    return 0;
 }
 
