31020661608e06afed512f5e2972b12dbf69af4a
gh6sty6@mail.ru <gh6sty6@mail.ru>
2024-09-01 22:35:50 +0300

    Реализованы функции заполнения и вывода векторов для длинных чисел

--- long_arithmetic/long_arithmetic.cpp
+++ long_arithmetic/long_arithmetic.cpp
@@ -4,35 +4,47 @@
 #include <string>
 using namespace std;
 
+//функция для заполнения вектора
+vector<int> read_long_number() {
+    string num_str;
+    cout << "Введите число: ";
+    cin >> num_str;
+
+    vector<int> num_vector;
+
+    // Преобразуем строку в вектор цифр
+    for (char c : num_str) {
+        // Проверяем, что символ является цифрой
+        if (isdigit(c)) 
+            num_vector.push_back(c - '0'); // Преобразуем символ в число и добавляем в вектор       
+    }
+
+    return num_vector;
+}
+
+//функция вывода вектора
+void print_vector(const vector<int>& num_vector) {
+    for (int digit : num_vector) 
+        cout << digit;  
+
+    cout << endl;
+}
+
 int main()
 {
     setlocale(LC_ALL, "Ru");
-    vector<int> a;
-    vector<int> b;
-    string input;
+    vector<int> a = read_long_number();
+    vector<int> b = read_long_number();
 
-    cout << "Введите числа для числа a, разделенные пробелами, и нажмите enter: ";
-    getline(cin, input);
-    istringstream stream(input);
-    int num;
-    while (stream >> num) {
-        a.push_back(num);
-    }
+    cout << "\nЧисло 1: ";
+    print_vector(a);
 
-    cout << "\nВектор 1: ";
-    for (auto x : a)
-        cout << x << " ";
+    cout << "\nЧисло 2: ";
+    print_vector(b);
 
-    cout << "\nВведите числа для числа b, разделенные пробелами, и нажмите enter: ";
-    getline(cin, input);
-    istringstream stream2(input);
-    while (stream2 >> num) {
-        b.push_back(num);
-    }
 
-    cout << "\nВектор 2: ";
-    for (auto x : b)
-        cout << x << " ";
+
+
 
 }
 
