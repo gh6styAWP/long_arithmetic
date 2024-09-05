c298af38cf31102d7447f5973d2e83c3ff266287
gh6sty6@mail.ru <gh6sty6@mail.ru>
2024-09-05 21:40:49 +0300

    Реализованы создание двух пустых вектором, заполнение строки символов с клавиатуры и передача символов строки в векторы.

--- long_arithmetic/long_arithmetic.cpp
+++ long_arithmetic/long_arithmetic.cpp
@@ -13,10 +13,25 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Ru");
 
+    //создаем два вектора для двух длиннх чисел
+    vector<char> num1;
+    vector<char> num2;
+    string input1, input2;
 
+    cout << "Введите первое число: ";
+    cin >> input1;
 
+    //заполнение вектора введенными числами
+    for (char c : input1)
+        num1.push_back(c);
 
-    
+    cout << "\nВведите второе число: ";
+    cin >> input2;
+
+    for (char c : input2)
+        num2.push_back(c);
+
+   
 
     return 0;
 }
